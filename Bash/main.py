5b73a044a08ecc09f0e5b2977a3e392b71fb6f4f
juchang <c2w2@naver.com>
2018-11-16 22:30:30 +0900

    Add: Bash

--- Bash/main.py
+++ Bash/main.py
@@ -1,5 +1,8 @@
+#!/usr/bin/python2.7
 import sys
 import signal
+import random
+import subprocess
 
 def send(data):
     sys.stdout.write(data)
@@ -12,6 +15,12 @@ def recv():
     data = raw_input(':> ')
     return data
 
+def getRand(table, length):
+    res = ''
+    for i in xrange(length):
+        res += random.choice(table)
+    return res
+
 def filter(data):
     filtering = []
     for i in filtering:
@@ -23,16 +32,46 @@ def timeout():
     sendline("Timeout!")
     sys.exit(1)
 
+def execute(cmd):
+    fileheader  = '#!/bin/bash\n'
+    code = fileheader + cmd + '\n'
+
+    filename = 'SH_' + getRand(__import__('string').lowercase, 16) + '.sh'
+
+    f = open('/tmp/' + filename, 'w')
+    f.write(code)
+    f.close()
+
+    subprocess.call(['chmod','+x','/tmp/'+filename])
+
+    res = subprocess.check_output(['/tmp/' + filename])
+
+    return res
+
 if __name__ == '__main__':
     signal.signal(signal.SIGALRM, timeout)
-    signal.alarm(240)
+    signal.alarm(60)
 
+    sendline("Plz send SH code")
+    sendline("If you send \"END\", input will be ended")
+    sendline("Then i will execute your code")
+
+    sendline('-'*50)
+    sendline('#!/bin/bash')
+   
+    code = ''
     while True:
         cmd = recv()
-        if filter(cmd):
-            try:
-                # Eval command
-            except:
-                sendline("Error")
-        else:
-            sendline("Filtered")
+        if "END" in cmd:
+            break
+        code += cmd
+    
+    sendline('-'*50)
+
+    if filter(code):
+        try:
+            print execute(code)
+        except:
+            sendline("Error")
+    else:
+        sendline("Filtered")
