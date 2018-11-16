71342f05d48154a123a3ee36c79e20cd3be90579
juchang <c2w2@naver.com>
2018-11-16 21:41:20 +0900

    Fix: run.sh

--- run.sh
+++ run.sh
@@ -4,9 +4,9 @@ prog=('Bash' 'C' 'Go' 'Js' 'Perl' 'PHP' 'Python' 'Ruby')
 port=(6656 6657 6658 6659 6666 6667 6668 6669)
 
 for index in ${!prog[*]} ; do
-    path="`pwd`/jail/${prog[$index]}"
-    cmd="docker build -t prob/${prog[$index]} ${path}"
+    path="`pwd`/${prog[$index]}"
+    cmd="docker build -t prob/${prog[$index],,} ${path}"
     $cmd
-    runner="docker run -d -p ${port[$index]}:2222 prob/${prog[$index]}"
+    runner="docker run -d -p ${port[$index]}:2222 prob/${prog[$index],,}"
     $runner
 done
